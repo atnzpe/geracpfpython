77d885be361b57b4204fe89bf8d9318f77f552ce
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-09-19 18:22:25 -0300

    Ajuste final arquivo main

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 ##Do arquivo functions.py importamos todas as funções
 from functions import *
 
-print(nine_first())
-print(validate_penultimate_number(nine_first()))
+#Define os campos do CPF
 
-nove_primeiros = nine_first()
-penultimo_numero = validate_penultimate_number(nove_primeiros)
-
-cpf = f"{nove_primeiros}{penultimo_numero}"
-
-print(cpf)
-print(cpf)
-
-
-for i in range(10):
+numbers_cpf = int(input('Digite quantos CPF você quer gerar: '))
+lista_cpf_gerados: []
+for i in range(numbers_cpf):
+    
     nove_primeiros = nine_first()
     penultimo_numero = validate_penultimate_number(nove_primeiros)
-
-    cpf = f"{nove_primeiros}{penultimo_numero}"
-
+    ultimo_numero = validate_last_number(nove_primeiros)
+    cpf = f"{nove_primeiros}{penultimo_numero}{ultimo_numero}"
     print(cpf)
+    #lista_cpf_gerados.append(cpf)
+
